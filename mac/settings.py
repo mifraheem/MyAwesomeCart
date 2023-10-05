5dfa18d79d1f3d6f76f6df289d41148489efe667
M Ifraheem <mifrahim017@gmail.com>
2023-10-05 15:05:02 +0500

    static url updated

--- mac/settings.py
+++ mac/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # managing media of project....
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
